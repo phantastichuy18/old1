c79833b7052b9a0a1d606fe3e9539593064af104
hello <duchuydam54@gmail.com>
2023-07-17 13:23:30 +0700

    Add files via upload

---
+++ adpls.js
@@ -0,0 +1,18 @@
+function adBlockCheck() {
+    setTimeout(function() {
+        document.body.innerHTML += `<div class="adsbygoogle" id="test-ad"></div>`;
+        const testAd = document.getElementById('test-ad');
+        const testAdStyle = getComputedStyle(testAd);
+
+        if (testAdStyle.display === 'none') {	
+            const banner = document.createElement('div');
+            banner.innerHTML = `
+            <h3>Ad blocker detected</h3>
+            <p>To keep our services free, we use advertising on this site. Please consider supporting us by disabling your ad blocker.</p>
+            `
+            banner.setAttribute('class', 'alert');
+            banner.style = "background: red; color: whitesmoke; width: 100%; padding: 0.8rem; font-size: 1.2rem";
+            document.body.prepend(banner);
+        } 
+    },1000)
+}

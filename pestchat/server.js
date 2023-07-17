a0dae2894f6091c7ff88c694a28c1f05f0c3d69f
hello <duchuydam54@gmail.com>
2023-07-17 21:14:58 +0700

    Update server.js

--- pestchat/server.js
+++ pestchat/server.js
@@ -4,13 +4,17 @@ const WebSocket = require('ws');
 
 const port = 6969;
 const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
+    // Convert the message to a string.
+    const message = data.toString();
+
+    // Send the message to all of the clients.
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+        client.send(message);
       }
     })
   })
@@ -19,4 +23,3 @@ wss.on('connection', function connection(ws) {
 server.listen(port, function() {
   console.log(`Server is listening on ${port}!`)
 })
-

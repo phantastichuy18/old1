da7a4cb3f3d5f7f23949cd38a65a2ab8c66db42f
hello <duchuydam54@gmail.com>
2023-07-18 19:37:01 +0700

    Update pestchat.js

--- pestchat/pestchat.js
+++ pestchat/pestchat.js
@@ -158,6 +158,8 @@ window.onload = function() {
         chat_input_send.setAttribute('id', 'chat_input_send')
         chat_input_send.setAttribute('disabled', true)
         chat_input_send.innerHTML = `<i class="far fa-paper-plane"></i>`
+        chat_input_send.style.width = '25px';
+        chat_input_send.style.height = '45px';
   
         var chat_input = document.createElement('input')
         chat_input.setAttribute('id', 'chat_input')
